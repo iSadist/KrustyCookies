57b0213d07b9003ef23e35a22116c21b10a10026
Jan Svensson <jsvensson51@yahoo.com>
2018-03-24 11:36:39 +0100

    Add orders in the database at start

--- SQL/orders.sql
+++ SQL/orders.sql
@@ -1,4 +1,14 @@
+-- Add orders
+
 INSERT INTO orders VALUES (1, 'Skånekakor AB', CURRENT_DATE);
 INSERT INTO orders VALUES (2, 'Skånekakor AB', CURRENT_DATE);
 INSERT INTO orders VALUES (3, 'Finkakor AB', CURRENT_DATE);
 INSERT INTO orders VALUES (4, 'Bjudkakor AB', CURRENT_DATE);
+
+-- Add products for the orders
+
+INSERT INTO recipes_order VALUES ("Nut ring", 1, 2);
+INSERT INTO recipes_order VALUES ("Tango", 2, 10);
+INSERT INTO recipes_order VALUES ("Tango", 3, 5);
+INSERT INTO recipes_order VALUES ("Berliner", 3, 15);
+INSERT INTO recipes_order VALUES ("Nut cookie", 4, 200);
